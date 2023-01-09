0030400ddc818e56af437098b26b91066b7a62fc
AnayaCrenshaw <121388200+AnayaCrenshaw@users.noreply.github.com>
2023-01-08 20:32:52 -0500

    made some more changes to the js and html

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "e4599e9ea17c93ffbb1e1f3e290a3806";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
